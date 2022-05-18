8925f6d9c14f2907cd79be1368651f81ce67ad71
Geovany <marlongeo1999@gmail.com>
2022-05-17 23:31:29 -0600

    added some test about buttons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import "./App.css";
 import { useState } from "react";
 
-function TodoItems({ task, setTask }) {
+export function TodoItems({ index, task, complete_task, remove_todo }) {
   return (
     <>
       <ul>
         {task.map((tk, index) => {
           return (
             <div key={index} className="todo__item">
-              <li>{tk.task}</li>
+              <li
+                style={{ textDecoration: tk.isCompleted ? "line-through" : "" }}
+              >
+                {tk.task}
+              </li>
               <div className="btns">
-                <button className="add">Complete</button>
-                <button className="add">Delete</button>
+                <button className="add" onClick={() => complete_task(index)}>
+                  Complete {index}
+                </button>
+                <button className="delete" onClick={() => remove_todo(index)}>
+                  Delete
+                </button>
               </div>
             </div>
           );
@@ -28,15 +36,63 @@ function App() {
     { task: "Complete the course", isCompleted: false },
   ];
 
-  const [task, setTask] = useState(initialV);
+  const [tasks, setTask] = useState(initialV);
+
+  const add_item = (task) => {
+    const newItem = [{ task }, ...tasks];
+    console.log("item", task, "new", newItem);
+    setTask(newItem);
+  };
+
+  const complete_task = (index) => {
+    const newItem = [...tasks];
+
+    !newItem[index].isCompleted
+      ? (newItem[index].isCompleted = true)
+      : (newItem[index].isCompleted = false);
+
+    setTask(newItem);
+  };
+
+  const remove_todo = (index) => {
+    const newItems = [...tasks];
+    newItems.splice(index, 1);
+    setTask(newItems);
+  };
+
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!value) return;
+    add_item(value);
+    setValue("");
+  };
+
+  const handleOnChange = (e) => {
+    console.log("value", e.target.value, "value", value);
+    setValue(e.target.value);
+  };
 
   return (
     <div className="App">
       <h1>🧪J E S T 🧪</h1>
-      <form className="todo__form">
-        <input placeholder="Enter new task"></input>
+      <form className="todo__form" onSubmit={handleSubmit}>
+        <input
+          placeholder="Enter new task"
+          type="text"
+          className="entry__task"
+          onChange={handleOnChange}
+          value={value}
+        ></input>
         <button className="add__task">Add</button>
-        <TodoItems task={task} setTask={setTask}></TodoItems>
+        <TodoItems
+          task={tasks}
+          setTask={setTask}
+          complete_task={complete_task}
+          remove_todo={remove_todo}
+        ></TodoItems>
       </form>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { createEvent, fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App, { TodoItems } from "./App";
 
-describe('buttons working', () => {
-  test('complete button exist', () => {
+describe("APP function TODOITEMS", () => {
+  test("ejecuta complete", async () => {
     render(<App />);
-    const completeBtn = screen.getByText(/Complete/i);
-    expect(completeBtn).toBeInTheDocument();
-  });
+    const completeTodo = jest.fn();
+    const removeTodo = jest.fn();
 
-  test('delete button exist', () => {
-    render(<App/>);
-    const deleteBtn = screen.getAllByText(/Create/i);
+    const index = 5;
+    const task = [
+      {
+        task: "Luna dame bread",
+        isCompleted: true,
+      },
+    ];
 
-    expect(deleteBtn).toBeInTheDocument();
+    render(
+      <TodoItems
+        task={task}
+        index={index}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+      />
+    );
+    const completeBtn = screen.getAllByText("Complete 0")[1];
+    const removeBtn = screen.getAllByText("Delete")[0];
+
+    await userEvent.click(completeBtn);
+    await userEvent.click(removeBtn);
+
+    expect(removeTodo.mock.calls).toEqual([]);
   });
-  
-})
+});
